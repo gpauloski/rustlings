65aba109240374e354a470b31b6493ec659dd4c6
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2024-06-24 08:44:51 -0500

    Complete exercises

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -41,10 +41,30 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
-    fn from(s: &str) -> Person {}
+    fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default()
+        }
+
+        let parts: Vec<&str> = s.split(",").collect();
+        match parts.len() {
+            2 => {
+                let (name, age_str) = (parts[0], parts[1]);
+                if name.len() == 0 {
+                    Person::default()
+                } else if let Ok(age) = age_str.parse::<usize>() {
+                    Person {
+                        name: name.into(),
+                        age,
+                    }
+                } else {
+                    Person::default()
+                }
+            },
+            _ => Person::default()
+        }
+    }
 }
 
 fn main() {
--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -48,6 +46,24 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty)
+        }
+        
+        let parts: Vec<_> = s.split(",").collect();
+        if parts.len() != 2 {
+            return Err(ParsePersonError::BadLen)
+        }
+
+        let (name, age_str) = (parts[0], parts[1]);
+        if name.len() == 0 {
+            return Err(ParsePersonError::NoName)
+        }
+
+        match age_str.parse::<usize>() {
+            Ok(age) => Ok(Person { name: name.to_string(), age }),
+            Err(e) => Err(ParsePersonError::ParseInt(e)),
+        }
     }
 }
 
